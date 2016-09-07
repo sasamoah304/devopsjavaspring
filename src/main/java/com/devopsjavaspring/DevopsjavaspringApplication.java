e956ce5c646c1e36dc6ee9e727b2c5a969a12d4b
Stephen Asamoah <sasamoah@me.com>
2016-09-07 09:45:17 -0400

    Initial Helloworld commit

---
+++ src/main/java/com/devopsjavaspring/DevopsjavaspringApplication.java
@@ -0,0 +1,12 @@
+package com.devopsjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsjavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsjavaspringApplication.class, args);
+	}
+}

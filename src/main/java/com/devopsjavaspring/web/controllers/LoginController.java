e536347774b4fcf42c2e4468f4b618a95dd94c13
Stephen Asamoah <sasamoah@me.com>
2016-09-08 14:37:18 -0400

    Added Login functionality

---
+++ src/main/java/com/devopsjavaspring/web/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.devopsjavaspring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by stephenasamoah on 9/8/16.
+ */
+
+@Controller
+public class LoginController {
+
+    /** The login view name */
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login(){
+        return LOGIN_VIEW_NAME;
+    }
+}

457af051b6a13a7be1ba84a20279e8fb94d1555d
Stephen Asamoah <sasamoah@me.com>
2016-09-13 11:40:31 -0400

    Created signup form and handler method

--- src/main/java/com/devopsjavaspring/config/SecurityConfig.java
+++ src/main/java/com/devopsjavaspring/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.devopsjavaspring.config;
 
 import com.devopsjavaspring.backend.service.UserSecurityService;
 import com.devopsjavaspring.web.controllers.ForgotMyPasswordController;
+import com.devopsjavaspring.web.controllers.SignupController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,7 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/error/**/*",
             "/console/**",
             ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING,
-            ForgotMyPasswordController.CHANGE_PASSWORD_PATH
+            ForgotMyPasswordController.CHANGE_PASSWORD_PATH,
+            SignupController.SIGNUP_URL_MAPPING
     };
 
     @Override
